644da17cda5327c6dfe74404198b866e54fa746c
mkabumattar <mohammad.khaled@outlook.com>
2021-12-29 17:14:02 +0200

    build api

---
+++ data/java/pattern_1/code.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner createScanner = new Scanner(System.in);
+        int num;
+
+        System.out.print("Enter the number of rows and columns for the square: ");
+        num = createScanner.nextInt();
+
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= num; j++) {
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
