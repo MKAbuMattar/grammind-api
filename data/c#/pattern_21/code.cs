2e0b3c8aaf9bf976e1a10a00b3d4ff9ab47bb769
mkabumattar <mohammad.khaled@outlook.com>
2022-01-10 23:17:13 +0200

    dev work add c# patterns

---
+++ data/c#/pattern_21/code.cs
@@ -0,0 +1,19 @@
+using System;
+class Grammind
+{
+    static void Main()
+    {
+        int num;
+        Console.Write("Enter the number of rows and columns for the square: ");
+        num = Int32.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= num; i++)
+        {
+            for (int j = 0; j < num; j++)
+            {
+                Console.Write("{0,2:D }", (i * j) % 2);
+            }
+            Console.Write("\n");
+        }
+    }
+}
---
+++ data/c#/pattern_24/main.cs
@@ -0,0 +1,26 @@
+using System;
+class Grammind
+{
+    static void Main()
+    {
+        int num;
+        string input;
+        Console.Write("Enter the number of rows and columns for the square: ");
+        input = Console.ReadLine();
+
+        while (!int.TryParse(input, out num) || num <= 0)
+        {
+            Console.Write("Must the number be positive, re-enter the number of rows and columns for the square: ");
+            input = Console.ReadLine();
+        }
+
+        for (int i = 1; i <= num; i++)
+        {
+            for (int j = 0; j < num; j++)
+            {
+                Console.Write("{0,2:D }", j % 2);
+            }
+            Console.Write("\n");
+        }
+    }
+}
